d66aad02c6255dba898751b8a81c8436db108be7
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-05-21 18:30:17 -0600

    Fix width for wide screens

--- components/Grass.tsx
+++ components/Grass.tsx
@@ -5,7 +5,7 @@ import styles from "../styles/Window.module.scss";
 export const Grass = () => {
 	return (
 		<div className={styles.grass}>
-			<Image src='/images/grass.png' height={120} width={2000} alt='gras' />;
+			<Image src='/images/grass.png' height={120} width={2800} alt='gras' />;
 		</div>
 	);
 };
