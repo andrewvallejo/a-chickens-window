3f6b54bccd7df2e5bf87a0be9bccc5a6f641de32
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-05-22 13:56:20 -0600

    Add animations when visiting the landing page

--- components/math/MathProblem.tsx
+++ components/math/MathProblem.tsx
@@ -1,4 +1,4 @@
-import { createRef, useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 import { useGameContext } from "../../context/GameContext";
 import styles from "../../styles/Math.module.scss";
@@ -10,12 +10,29 @@ export const MathProblem = (): JSX.Element => {
 	const { result, addends } = useGameContext();
 
 	return (
-		<section className={styles.mathContainer}>
-			<Block>{addends[0]}</Block>
-			<Operator>+</Operator>
-			<Block>{addends[1]}</Block>
-			<Operator>=</Operator>
-			<FormBlock>{result}</FormBlock>
-		</section>
+		<motion.div
+			initial='hidden'
+			animate='visible'
+			variants={{
+				hidden: { opacity: 0, translateY: "400px" },
+				visible: {
+					opacity: 1,
+					translateY: 0,
+					transition: {
+						delay: 2.35,
+						duration: 1,
+						type: "spring",
+						stiffness: 85
+					}
+				}
+			}}>
+			<section className={styles.mathContainer}>
+				<Block>{addends[0]}</Block>
+				<Operator>+</Operator>
+				<Block>{addends[1]}</Block>
+				<Operator>=</Operator>
+				<FormBlock>{result}</FormBlock>
+			</section>
+		</motion.div>
 	);
 };
--- components/window/Window.tsx
+++ components/window/Window.tsx
@@ -1,17 +1,26 @@
+import { motion } from "framer-motion";
+
 import styles from "../../styles/Window.module.scss";
-import { Chicken } from "../Chicken";
+import { Chicken } from "./Chicken";
 import { Cloud } from "./Cloud";
 import { Grass } from "./Grass";
 
 export const Window = (): JSX.Element => {
 	return (
-		<section className={styles.window}>
+		<motion.section
+			className={styles.window}
+			initial='hidden'
+			animate='visible'
+			variants={{
+				hidden: { scale: 0.8, opacity: 0, width: 0 },
+				visible: { scale: 1, opacity: 1, width: "100%", transition: { delay: 0.25, duration: 1.25 } }
+			}}>
 			<div className={styles.sun} />
 			<Cloud>high</Cloud>
 			<Cloud>low</Cloud>
 			<Grass>
 				<Chicken />
 			</Grass>
-		</section>
+		</motion.section>
 	);
 };
