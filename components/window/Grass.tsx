11b2a48cef32b62c17e96a1d605b8d25b3fc79c1
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-05-22 10:59:31 -0600

    Update chicken spawn to a grid

--- components/window/Grass.tsx
+++ components/window/Grass.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 
 import { useGameContext } from "../../context/GameContext";
-import chickens from "../../styles/Chicken.module.scss";
 import styles from "../../styles/Window.module.scss";
 
 export const Grass = ({ children }: { children: JSX.Element }): JSX.Element => {
@@ -9,17 +8,21 @@ export const Grass = ({ children }: { children: JSX.Element }): JSX.Element => {
 
 	return (
 		<div className={styles.grass}>
-			{level > 0 && <div className={chickens.peter}>{children}</div>}
-			{level > 1 && <div className={chickens.mary}>{children}</div>}
-			{level > 2 && <div className={chickens.andrew}>{children}</div>}
-			{level > 3 && <div className={chickens.eron}>{children}</div>}
-			{level > 4 && <div className={chickens.roy}>{children}</div>}
-			{level > 5 && <div className={chickens.bob}>{children}</div>}
-			{level > 6 && <div className={chickens.maroni}>{children}</div>}
-			{level > 7 && <div className={chickens.bobJr}>{children}</div>}
-			{level > 8 && <div className={chickens.joshua}>{children}</div>}
-			{level > 9 && <div className={chickens.shayan}>{children}</div>}
-			<Image src='/images/grass.png' height={120} width={2800} alt='gras' />;
+			<Image src='/images/grass.png' height={200} width={2400} alt='grass' />
+			<div className={styles.spawnGrid}>
+				{level > 0 && <div> {children}</div>}
+				{level > 1 && <div> {children}</div>}
+				{level > 2 && <div> {children}</div>}
+				{level > 3 && <div> {children}</div>}
+				{level > 4 && <div> {children}</div>}
+			</div>
+			<div className={styles.spawnGridTwo}>
+				{level > 5 && <div> {children}</div>}
+				{level > 6 && <div> {children}</div>}
+				{level > 7 && <div> {children}</div>}
+				{level > 8 && <div> {children}</div>}
+				{level > 9 && <div> {children}</div>}
+			</div>
 		</div>
 	);
 };
