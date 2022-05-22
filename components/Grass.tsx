5263e5d9a68e145b35e96802d95bef4deab604ec
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-05-21 21:45:39 -0600

    Add chickens stylesheet

--- components/Grass.tsx
+++ components/Grass.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 
+import chickens from "../styles/Chicken.module.scss";
 import styles from "../styles/Window.module.scss";
 
 export const Grass = ({ children }: { children: JSX.Element }) => {
 	return (
 		<div className={styles.grass}>
-			<div className={styles.peter}>{children}</div>
-			<div className={styles.mary}>{children}</div>
+			<div className={chickens.peter}>{children}</div>
+			<div className={chickens.mary}>{children}</div>
 			<Image src='/images/grass.png' height={120} width={2800} alt='gras' />;
 		</div>
 	);
