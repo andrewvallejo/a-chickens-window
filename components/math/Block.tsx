8c5f260a77fdebf92a845d6b1b6cc8d2c00fd203
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-05-22 17:10:32 -0600

    Form is now nested inside of block

--- components/math/Block.tsx
+++ components/math/Block.tsx
@@ -1,9 +1,21 @@
+import { motion } from "framer-motion";
+
 import styles from "../../styles/Math.module.scss";
+import { FormBlock } from "./FormBlock";
 
 export const Block = ({ children }: { children: number }): JSX.Element => {
 	return (
-		<article className={styles.block}>
+		<motion.article
+			animate={{
+				y: [ 8, 4, 8 ]
+			}}
+			transition={{
+				repeat: Infinity,
+				duration: 5
+			}}
+			className={styles.block}>
 			<p className={styles.number}>{children}</p>
-		</article>
+			<FormBlock>{children}</FormBlock>
+		</motion.article>
 	);
 };
