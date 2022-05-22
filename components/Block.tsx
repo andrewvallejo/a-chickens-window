0549c8d3002103562fefa1a5f85567cec3e16f92
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-05-22 10:15:01 -0600

    Organize files one folder deeper

--- components/Block.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "../styles/MathProblem.module.scss";
-
-export const Block = ({ children }: { children: number }) => {
-	return (
-		<article className={styles.block}>
-			<p className={styles.number}>{children}</p>
-		</article>
-	);
-};
--- components/Operator.tsx
+++
@@ -1,5 +0,0 @@
-import styles from "../styles/MathProblem.module.scss";
-
-export const Operator = ({ children }: { children: string }) => {
-	return <p className={styles.operator}>{children}</p>;
-};
