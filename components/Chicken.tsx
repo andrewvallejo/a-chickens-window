a1178a7d84fcf9ba31ba4026f479d071a0f7f8bf
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-05-22 13:17:14 -0600

    Add drag&drop interactions to chkn

--- components/Chicken.tsx
+++ components/Chicken.tsx
@@ -1,13 +1,29 @@
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 import styles from "../styles/Window.module.scss";
 
 export const Chicken = (): JSX.Element => {
 	return (
-		<div className={styles.chicken}>
-			<div className={styles.spritesheet}>
-				<Image src='/images/chicken-sheet.png' height={280} width={656} alt='chicken' />
+		<motion.div
+			whileTap={{
+				scale: 1.2,
+				transition: {
+					duration: 0.2,
+					ease: "easeInOut"
+				}
+			}}
+			drag={true}
+			dragConstraints={{
+				top: 20,
+				bottom: 30
+			}}
+			dragElastic={0.2}>
+			<div className={styles.chicken}>
+				<div className={styles.spritesheet}>
+					<Image src='/images/chicken-sheet.png' height={280} width={656} alt='chicken' />
+				</div>
 			</div>
-		</div>
+		</motion.div>
 	);
 };
