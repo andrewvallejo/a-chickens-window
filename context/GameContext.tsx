2f9eda1567236b50b5febbc7c9771e18340b6668
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-05-24 12:17:06 -0600

    Move isGameOver hook into context

--- context/GameContext.tsx
+++ context/GameContext.tsx
@@ -1,23 +1,27 @@
 import { createContext, useContext, useMemo, useState } from "react";
 
 const GameContext = createContext<{
-	setPending: (pending: boolean) => void;
+	pending: boolean;
+	isGameOver: boolean;
 	level: number;
 	addends: [number, number];
 	result: number;
 	answer: string;
-	pending: boolean;
+	setPending: (pending: boolean) => void;
+	setIsGameOver: (isGameOver: boolean) => void;
 	setLevel: (level: number) => void;
 	setAddends: (addends: [number, number]) => void;
 	setResult: (result: number) => void;
 	setAnswer: (answer: string) => void;
 }>({
-	setPending: () => {},
+	pending: true,
+	isGameOver: false,
 	level: 0,
 	addends: [ 0, 0 ],
 	answer: "",
 	result: 0,
-	pending: true,
+	setPending: () => {},
+	setIsGameOver: () => {},
 	setLevel: () => {},
 	setAddends: () => {},
 	setResult: () => {},
@@ -26,14 +30,28 @@ const GameContext = createContext<{
 
 export const GameWrapper = ({ children }: { children: React.ReactNode }): JSX.Element => {
 	const [ pending, setPending ] = useState<boolean>(false);
+	const [ isGameOver, setIsGameOver ] = useState<boolean>(false);
 	const [ level, setLevel ] = useState<number>(0);
 	const [ result, setResult ] = useState<number>(0);
 	const [ addends, setAddends ] = useState<[number, number]>([ 0, 0 ]);
 	const [ answer, setAnswer ] = useState<string>("");
 
 	const context = useMemo(
-		() => ({ pending, level, addends, result, answer, setPending, setLevel, setAddends, setResult, setAnswer }),
-		[ level, addends, result, answer, pending ]
+		() => ({
+			pending,
+			isGameOver,
+			level,
+			addends,
+			result,
+			answer,
+			setPending,
+			setIsGameOver,
+			setLevel,
+			setAddends,
+			setResult,
+			setAnswer
+		}),
+		[ pending, isGameOver, level, addends, result, answer ]
 	);
 
 	return <GameContext.Provider value={context}>{children}</GameContext.Provider>;
