f050dc98c6514730f03ecd8e333c53669705e414
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-05-24 12:34:49 -0600

    Add animation game over

--- components/math/MathProblem.tsx
+++ components/math/MathProblem.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 import { useGameContext } from "../../context/GameContext";
 import styles from "../../styles/Math.module.scss";
@@ -9,29 +9,34 @@ export const MathProblem = (): JSX.Element => {
 	const { answer, addends } = useGameContext();
 
 	return (
-		<motion.section
-			className={styles.mathContainer}
-			initial='hidden'
-			animate='visible'
-			variants={{
-				hidden: { opacity: 0, translateY: "400px" },
-				visible: {
-					opacity: 1,
-					translateY: 0,
-					transition: {
-						staggerChildren: 1,
-						delay: 2.35,
-						duration: 1,
-						type: "spring",
-						stiffness: 85
+		<AnimatePresence>
+			<motion.section
+				className={styles.mathContainer}
+				initial='hidden'
+				animate='visible'
+				variants={{
+					hidden: {
+						opacity: 0,
+						translateY: "400px"
+					},
+					visible: {
+						opacity: 1,
+						translateY: 0,
+						transition: {
+							staggerChildren: 1,
+							delay: 2.35,
+							duration: 1,
+							type: "spring",
+							stiffness: 85
+						}
 					}
-				}
-			}}>
-			<Block>{[ addends[0], "addend" ]}</Block>
-			<Operator>+</Operator>
-			<Block>{[ addends[1], "addend" ]}</Block>
-			<Operator>=</Operator>
-			<Block>{[ parseInt(answer), "form" ]}</Block>
-		</motion.section>
+				}}>
+				<Block>{[ addends[0], "addend" ]}</Block>
+				<Operator>+</Operator>
+				<Block>{[ addends[1], "addend" ]}</Block>
+				<Operator>=</Operator>
+				<Block>{[ parseInt(answer), "form" ]}</Block>
+			</motion.section>
+		</AnimatePresence>
 	);
 };
